bd0a9b37ee7b0d1a2a48cd76a6926444800d50df
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-11-20 09:09:45 -0700

    complete with crud

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,7 @@ const Dashboard = (props) => {
     const { globalState, setGlobalState } = useContext(GlobalCtx);
     const { url, token } = globalState;
     const [notes, setNotes] = useState(null);
+    const [updateId, setUpdateId] = useState(null);
     const getNotes = async() => {
         const response = await fetch(url + "/note/", {
             method: 'get',
@@ -19,7 +20,8 @@ const Dashboard = (props) => {
         getNotes()
     }, []);
     //pulling from dom
-    const input = useRef(null)
+    const input = useRef(null);
+    const update = useRef(null);
     const handleClick = () => {
         console.log(input)
         const note = input.current.value
@@ -37,18 +39,56 @@ const Dashboard = (props) => {
                 getNotes()
             })
     }
+    const handleUpdate = () => {
+        const note = update.current.value
+        fetch(url + "/note/" + updateId, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `bearer ${token}`
+            },
+            body: JSON.stringify({note})
+        })
+        .then(response=>response.json())
+            .then(data => {
+                update.current.value = '';
+                setUpdateId(null);
+                getNotes();
+            })
+    }
+    const handleDelete = (id) => {
+        fetch(url + "/note/" + id, {
+            method: "DELETE",
+            headers: {
+                Authorization: `bearer ${token}`
+            },
+        })
+        .then(response=>response.json())
+            .then(data => {
+                getNotes()
+            })
+    }
     return (
         <div>
             <h1>Dashboard</h1>
             <h2>New Note</h2>
             <input type="text" name="note" ref={input} />
             <button onClick={handleClick}>Create Note</button>
+            <h2>Update Note</h2>
+            <input type="text" name="note" ref={update} />
+            <button onClick={handleUpdate}>Update Note</button>
             <h2>Notes</h2>
             <ul>
                 {notes? notes.map((note) => {
                     return<li
-                    key={note._id}>
-                    {note.note}
+                    key={note._id}><h3>
+                            {note.note}</h3>
+                        <button onClick={() => {
+                            handleDelete(note._id)
+                        }}>Delete</button><button onClick={() => {
+                            setUpdateId(note._id)
+                            update.current.value = note.note;
+                        }}>Edit</button>
                     </li>
                 }):null}
             </ul>
